05b412870094072bbde2336b4659d6ca2a0f77c1
Andres Marmol <andres@turpl.dev>
2026-04-16 19:03:10 -0400

    fix: use new type helper

--- src/jsonb.ts
+++ src/jsonb.ts
@@ -1,12 +1,14 @@
-import { type Column, type InferColumnsDataTypes, type SQL, sql } from 'drizzle-orm'
+import { type Column, type SQL, sql } from 'drizzle-orm'
+import type { InferColumnDataType } from './types.js'
 
 export function jsonbBuildObject<T extends Record<string, Column | SQL>>(obj: T) {
   const mapped = Object.entries(obj).map(([k, v]) => sql`${k}::text, ${v}`)
   return sql<{
     [K in keyof T]: T[K] extends Column
-      ? InferColumnsDataTypes<{ [P in K]: T[K] }>[K]
+      ? InferColumnDataType<T[K]>
       : T[K] extends SQL<infer U>
         ? U
         : never
   }>`jsonb_build_object(${sql.join(mapped, sql`, `)})`
 }
+
